4df7a03229c5bed518afccb82ddd3fc8f817f6cc
@oieusouoede <edemarinho@outlook.com>
2022-06-05 00:19:38 -0300

    DEV: Mais coisa que o git tirou

---
+++ src/test/java/com/telefake/falemuito/FalemuitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telefake.falemuito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalemuitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
