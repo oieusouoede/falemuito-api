0c462d24dcdd436fabd2b0218d91c240ce3e986f
@oieusouoede <edemarinho@outlook.com>
2022-06-05 01:43:08 -0300

    DEV: Enum de uf

---
+++ src/main/java/com/telefake/falemuito/domain/enums/UFEnum.java
@@ -0,0 +1,31 @@
+package com.telefake.falemuito.domain.enums;
+
+public enum UFEnum {
+  AC,
+  AL,
+  AP,
+  AM,
+  BA,
+  CE,
+  DF,
+  ES,
+  GO,
+  MA,
+  MT,
+  MS,
+  MG,
+  PA,
+  PB,
+  PR,
+  PE,
+  PI,
+  RJ,
+  RN,
+  RS,
+  RO,
+  RR,
+  SC,
+  SE,
+  SP,
+  TO
+}
