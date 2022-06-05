7135e5bee56e2bec534ffa19e5645f1aeca8a63d
@oieusouoede <edemarinho@outlook.com>
2022-06-05 01:47:06 -0300

    DEV: Factory que retorna uma service pra calcular de acordo com o Plano

---
+++ src/main/java/com/telefake/falemuito/services/FaleMuitoServiceFactory.java
@@ -0,0 +1,44 @@
+package com.telefake.falemuito.services;
+
+import org.springframework.stereotype.Service;
+
+import com.telefake.falemuito.domain.enums.FaleMuitoEnum;
+import com.telefake.falemuito.services.impl.FaleMuito120Service;
+import com.telefake.falemuito.services.impl.FaleMuito30Service;
+import com.telefake.falemuito.services.impl.FaleMuito60Service;
+import com.telefake.falemuito.services.impl.SemPlanoService;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class FaleMuitoServiceFactory {
+
+  private FaleMuitoEnum plano;
+
+  public static FaleMuitoServiceFactory INSTANCE (FaleMuitoEnum plano){
+        return new FaleMuitoServiceFactory(plano);
+    } 
+
+    public IFaleMuitoService semPlano(){
+        return retornaPlano(FaleMuitoEnum.SEMPLANO);
+    }
+
+    public IFaleMuitoService comPlano(){
+        return retornaPlano(plano);
+    }
+
+    private IFaleMuitoService retornaPlano(FaleMuitoEnum plano){
+      switch (plano){
+        case FALEMUITO30:
+          return new FaleMuito30Service();
+        case FALEMUITO60:
+          return new FaleMuito60Service();
+        case FALEMUITO120:
+          return new FaleMuito120Service();
+        case SEMPLANO:
+        default:
+          return new SemPlanoService();
+    }
+  }
+}
