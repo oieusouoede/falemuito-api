3aba9ebb4ae11295289c96e5139d4c8fe0b1b805
@oieusouoede <edemarinho@outlook.com>
2022-06-05 01:48:10 -0300

    DEV: Controller com os endpoints de simular plano e obter localidades respectivamente

--- src/main/java/com/telefake/falemuito/controllers/FaleMuitoController.java
+++ src/main/java/com/telefake/falemuito/controllers/FaleMuitoController.java
@@ -6,16 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telefake.falemuito.domain.models.Localidade;
+import com.telefake.falemuito.dto.requests.RequestSimularDTO;
+import com.telefake.falemuito.dto.response.ResponseSimularDTO;
 import com.telefake.falemuito.repositories.ILocalidadeRepository;
+import com.telefake.falemuito.services.FaleMuitoServiceFactory;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class FaleMuitoController {
 
   private @Autowired ILocalidadeRepository localidadeRepo;
+
+    @PostMapping("/simular")
+    public ResponseEntity<ResponseSimularDTO> simularPlano(@RequestBody RequestSimularDTO request){
+      FaleMuitoServiceFactory factory = FaleMuitoServiceFactory.INSTANCE(request.getPlano());
+      
+      Double valorSemPlano = factory.semPlano().calcular(request.getTarifa(), request.getMinutos());
+      Double valorComPlano = factory.comPlano().calcular(request.getTarifa(), request.getMinutos());
+
+      return ResponseEntity
+        .ok()
+        .body(
+          ResponseSimularDTO.builder()
+                            .origem(request.getTarifa().getOrigem().getDdd())
+                            .destino(request.getTarifa().getDestino().getDdd())
+                            .minutos(request.getMinutos())
+                            .plano(request.getPlano())
+                            .valorComFaleMais(valorComPlano)
+                            .valorSemFaleMais(valorSemPlano)
+                            .build()
+    );
+  }
 
   @GetMapping("/localidades")
   public ResponseEntity<List<Localidade>> getLocalidades (){
